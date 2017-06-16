385873ff2df57dfd6d23b99d6575d95f27d5d37a
bluetata <dietime1943@gmail.com>
2017-06-16 11:30:35 +0800

    JD Apater is added
    Signed-off-by: bluetata <dietime1943@gmail.com>

---
+++ src/main/java/com/datacrawler/service/model/com/jd/JDLoginApater.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.datacrawler.service.model.com.jd;
+
+/**
+ * @author bluetata
+ *
+ */
+public class JDLoginApater {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
