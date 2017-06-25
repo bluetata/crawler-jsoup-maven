3e03e8762c09d76b9c0c3402a0c63278a8d85fe6
bluetata <dietime1943@gmail.com>
2017-06-25 22:47:00 +0800

    edit html by jsoup
    Signed-off-by: bluetata <dietime1943@gmail.com>

--- src/test/java/com/datacrawler/test/jsoup/JsoupTest.java
+++ src/test/java/com/datacrawler/test/jsoup/JsoupTest.java
@@ -1,10 +1,25 @@
 package com.datacrawler.test.jsoup;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
 public class JsoupTest {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+        
+        String d = "<span><div>test</div></span>";
+        Document doc = Jsoup.parse(d);
+        Element div = doc.select("div").first(); // <div></div>
+        div.html("<p>lorem ipsum</p>"); // <div><p>lorem ipsum</p></div>
+        div.prepend("<p>First</p>");
+        div.append("<p>Last</p>");
+        // now: <div><p>First</p><p>lorem ipsum</p><p>Last</p></div>
 
+        Element span = doc.select("span").first(); // <span>One</span>
+        span.wrap("<li><a href='http://example.com/'></a></li>");
+        // now: <li><a href="http://example.com"><span>One</span></a></li>
+        System.out.println(doc.html());
     }
 
 }
